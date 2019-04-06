25cdfcd7ed1158a5116d934137bcc2c12e3b3c27
fabian jorge <fcsolucion@gmail.com>
2019-04-06 12:10:32 -0500

    se creo persistencia de quices y preguntas

---
+++ app/Pregunta.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pregunta extends Model
+
+
+
+{
+   $fillable.
+
+    protected $fillable = [
+        'enunciado', 'respuesta',
+    'opcion1', 'opcion2', 'opcion3', 'opcion4'
+    ];
+    
+
+    public function quiz()
+    {
+        return $this->belongsTo('App\Quiz');
+    }
+}
--- routes/botman.php
+++ routes/botman.php
@@ -7,3 +7,37 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+
+$botman->hears('/start', function ($bot) {
+    $nombres = $bot->getUser()->getFirstName() ?: "desconocido";
+    $bot->reply('Hola ' . $nombres . ', bienvenido al bot SimpleQuizzes!');
+});
+$botman->fallback(function ($bot) {
+    $bot->reply("No entiendo que quieres decir, vuelve a intentarlo.");
+});
+
+
+$botman->hears('/ayuda', function ($bot) {
+    $ayuda = ['/ayuda' => 'Mostrar este mensaje de ayuda',
+              'acerca de|acerca' => 'Ver la información quien desarrollo este lindo bot',
+              'listar quizzes|listar' => 'Listar los cuestionarios disponibles',
+              'iniciar quiz <id>' => 'Iniciar la solución de un cuestionario',
+              'ver puntajes|puntajes' => 'Ver los últimos puntajes',
+              'borrar mis datos|borrar' => 'Borrar mis datos del sistema'];
+    
+    $bot->reply("Los comandos disponibles son:");
+
+    foreach($ayuda as $key=>$value)
+    {
+            $bot->reply($key . ": " . $value);
+    }
+});
+$botman->hears('acerca de|acerca', function ($bot) {
+    $msj = "Este bot de charla fue desarrollado por:\n".
+            "Jorge I. Meza <jorge.meza@ucaldas.edu.co>\n".
+            "Durante las Jornadas de Ingeniería de 2018\n".
+            "de la Universidad de Caldas.";
+
+    $bot->reply($msj);
+});
